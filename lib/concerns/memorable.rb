d79d7cf780397865395b747f6d7bb911e2d9eb65
Parker Catalano <catalano.work@gmail.com>
2017-12-01 16:44:15 +0000

    automatically backed up by learn

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,19 @@
+module Memorable
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    @@artists.count
+  end
+
+  # def reset_all
+  #   @@songs.clear
+  # end
+
+  def count
+    self.all.count
+  end
+
+end
