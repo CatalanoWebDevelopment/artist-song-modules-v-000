ce70e85330e4184296ecf212f870f9d7f66a3d8a
Parker Catalano <catalano.work@gmail.com>
2017-12-01 17:36:06 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,11 @@
 module Findable
 
-  @@artists = []
-
   def find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
 
-  def find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
 end
