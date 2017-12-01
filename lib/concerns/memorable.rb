2f8ce7df628ffec65a7822e6bd080b09cbf7c3b6
Parker Catalano <catalano.work@gmail.com>
2017-12-01 17:57:14 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,19 +1,26 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def reset_all
-    self.all.clear
+    def count
+      @@artists.count
+    end
+
+    # def reset_all
+    #   @@songs.clear
+    # end
+
+    def count
+      self.all.count
+    end
   end
 
-  def count
-    @@artists.count
-  end
-
-  # def reset_all
-  #   @@songs.clear
-  # end
-
-  def count
-    self.all.count
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 
 end
